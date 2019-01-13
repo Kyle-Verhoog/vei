53ab6ebd3138cf627aa6e1f2e3e979b11efe1db5
Kevin Suwala <kevin@suwala.me>
2019-01-13 18:08:21 -0500

    Finally got grammar to work

--- parseGrammarJson.rb
+++ parseGrammarJson.rb
@@ -8,6 +8,11 @@ terminals = json['terminals']
 nonterminals = json['nonterminals']
 prodRules = json['productionRules']
 
+if (prodRules.keys - nonterminals).count > 0 or (nonterminals - prodRules.keys).count > 0
+  puts "Warning: number of proudction rules differs from nonterminals!"
+  exit
+end
+
 if (prodRules.keys - nonterminals).count > 0
   puts "Warning: number of proudction rules differs from nonterminals!"
   exit
