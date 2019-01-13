7d9bde4937d462e0185856345983204bb7563542
Kevin Suwala <kevin@suwala.me>
2019-01-13 02:35:07 -0500

    proof of concept parsing cfg using course website stuff

--- src/main/scala/compiler/Compiler.scala
+++ src/main/scala/compiler/Compiler.scala
@@ -1,9 +1,17 @@
 package compiler
 
+import jlalr.Jlalr1
+
+import scala.io.Source
+
 object Compiler {
   type State = String
 
   def main(args: Array[String]) {
+    val cfg = Source.fromResource("sample.cfg").mkString
+    println(cfg)
+    Jlalr1.parse(cfg)
+
     if (args.length.equals(0)) {
       println("Must supply a file!")
       return
