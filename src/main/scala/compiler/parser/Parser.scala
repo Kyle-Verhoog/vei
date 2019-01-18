a8c30f14f210a82498d4683654000afa3dd077f2
Kevin Suwala <kevin@suwala.me>
2019-01-18 13:21:51 -0500

    finish parsing algorithm, add BOF and EOF to grammar (like the example has)

--- src/main/scala/compiler/parser/Parser.scala
+++ src/main/scala/compiler/parser/Parser.scala
@@ -10,7 +10,7 @@ object Parser {
   class CFG(
       val terminals: ListBuffer[String],
       val nonterminal: ListBuffer[String],
-      val prodRules: mutable.HashMap[String, ListBuffer[Array[String]]],
+      val prodRules: ListBuffer[Array[String]],
       val shiftActions: mutable.HashMap[Int, mutable.HashMap[String, Int]],
       val reduceActions: mutable.HashMap[Int, mutable.HashMap[String, Int]]
   )
@@ -20,36 +20,52 @@ object Parser {
       val children: ListBuffer[Tree[T]] = ListBuffer[Tree[T]]()
   )
 
-  def parse(cfg: CFG, tokens: ListBuffer[Token]): Unit = {
-    val nodeStack = ListBuffer[Tree[Token]]()
-    val stateStack = ListBuffer[Int]()
+  def parse(cfg: CFG, tokens: ListBuffer[Token]): ListBuffer[Tree[Token]] = {
+    var nodeStack = ListBuffer[Tree[Token]]()
+    var stateStack = ListBuffer[Int]()
 
     // start algorithm
-    nodeStack.append(new Tree[Token](new Token("START", "none")))
-    stateStack.append(0, -1) // TODO need -1 state
+    nodeStack.append(new Tree[Token](new Token("BOF", "none"))) // TODO dont hardcode BOF?
+    stateStack.append(0)
     for (token <- tokens) {
 
-      // TODO other condition
-      while (cfg.reduceActions.contains(stateStack.last)) {
-        val newState = cfg.reduceActions(stateStack.last)(token.value)
-        val A = 
+      while (cfg.reduceActions.contains(stateStack.last) && cfg
+               .reduceActions(stateStack.last)
+               .contains(token.value)) {
+        // get the production rule and split into parts
+        val ruleToReduce = cfg.reduceActions(stateStack.last)(token.value)
+        val prodRule = cfg.prodRules(ruleToReduce)
+        val A = prodRule(0) // look up the nonterminal that this production rule is
+        val gamma = prodRule.takeRight(prodRule.length - 1)
 
+        // pop |gamma| child nodes, and |gama| states
+        val childNodes = ListBuffer[Tree[Token]]()
+        for (i <- 0 until gamma.length) {
+          childNodes.append(nodeStack.last)
+          nodeStack = nodeStack.take(nodeStack.length - 1)
+          stateStack = stateStack.take(stateStack.length - 1)
+        }
 
+        nodeStack.append(new Tree[Token](new Token(A, "non-leaf"), childNodes))
 
+        if (!cfg.shiftActions.contains(stateStack.last) || !cfg.shiftActions(stateStack.last).contains(A)) {
+          throw new RuntimeException("bad parsing 1") // TODO better error
+        }
 
-
-        val tokens = cfg.reduceActions(stateStack.last)
-        val gamma = tokens.size
-
-        // pop things off the nodestack
-        val childNodes = nodeStack.takeRight(gamma).reverse
-        nodeStack.remove(nodeStack.size - gamma, gamma)
-
-        // TODO add stuff
-        //nodeStack.append(new Tree[Token](, childNodes))
+        stateStack.append(cfg.shiftActions(stateStack.last)(A))
       }
+
+      nodeStack.append(new Tree[Token](token))
+
+      // Error check
+      if (!cfg.shiftActions.contains(stateStack.last) || !cfg.shiftActions(stateStack.last).contains(token.value)) {
+        throw new RuntimeException("bad parsing 2") // TODO better error
+      }
+
+      stateStack.append(cfg.shiftActions(stateStack.last)(token.value))
     }
 
+    nodeStack
   }
 
   def readInLr1(file: String): Unit = {
@@ -77,19 +93,12 @@ object Parser {
 
     // ---- process production rules ----
     count = lines(0).toInt
-    val prodRules = mutable.HashMap[String, ListBuffer[Array[String]]]()
+    val prodRules = ListBuffer[Array[String]]()
     lines = lines.takeRight(lines.length - 1)
 
     for (i <- 0 until count) {
-      val line = lines(0).split(" ")
-      val key = line(0)
-      val value = line.takeRight(line.length - 1)
-
-      // make entry if needed
-      if (!prodRules.contains(key)) {
-        prodRules(key) = ListBuffer[Array[String]]()
-      }
-      prodRules(key).append(value)
+      prodRules.append(lines(0).split(" "))
+      lines = lines.takeRight(lines.length - 1)
     }
 
     // ---- process actions ----
