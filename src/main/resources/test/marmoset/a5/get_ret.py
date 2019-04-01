0ad07757a02fb519c61f9609ae95c86ca6a1659e
Kyle Verhoog <kyle@verhoog.ca>
2019-04-01 00:09:06 -0400

    codegen/tests: add result finder

---
+++ src/main/resources/test/marmoset/a5/get_ret.py
@@ -0,0 +1,45 @@
+import glob
+import os
+import subprocess
+import sys
+
+
+JAVA_SRC = """
+public class Runner {{
+  public static void main(String[] args) {{
+    System.out.println({}.test());
+  }}
+}}
+"""
+
+for f in os.listdir(os.getcwd()):
+    if os.path.isdir(f):
+        test_name = f
+        print(test_name)
+        files = glob.glob('{}/**/*.java'.format(f), recursive=True)
+        with open('Runner.java', 'w') as runner:
+            src = JAVA_SRC.format(test_name)
+            runner.write(src)
+    elif f.endswith('.java'):
+        test_name = f[:-5]
+        with open('Runner.java', 'w') as runner:
+            src = JAVA_SRC.format(test_name)
+            runner.write(src)
+    else:
+        test_name = ""
+        continue
+
+    print('processing {}'.format(test_name))
+    args = ['/usr/lib/jvm/java-10-openjdk/bin/javac', 'Runner.java']
+    r = subprocess.call(args)
+
+    args = ['java', 'Runner']
+    p = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = p.communicate()
+    ret = p.returncode
+
+    print(stdout)
+    print(stderr)
+
+    # with open('{}.ret'.format(test_name), 'wb') as retf:
+    #     retf.write(stdout)
