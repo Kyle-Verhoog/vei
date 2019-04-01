e7f6e43e6c991a1850fbcadaea5e97ee563ad8f6
Kevin Suwala <kevin@suwala.me>
2019-03-31 22:17:40 -0400

    Convert string to char

--- src/main/scala/compiler/joos1w/Joos1WCodeGen.scala
+++ src/main/scala/compiler/joos1w/Joos1WCodeGen.scala
@@ -486,4 +486,26 @@ object Joos1WCodeGen {
         }
     }
   }
+
+  def stringToChar(str: String): Char = {
+    str match {
+      case "'\''" => '\''
+      case "'\"'" => '\"'
+      case "'\\'" => '\\'
+      case "'\n'" => '\n'
+      case "'\r'" => '\r'
+      case "'\t'" => '\t'
+      case "'\b'" => '\b'
+      case "'\f'" => '\f'
+      case "'\0'" => '\0'
+      case _ => {
+        if (str.length != 3) {
+          throw new RuntimeException(
+            s"Do not know how to convert string:    ${str}    to char")
+        } else {
+          str(1)
+        }
+      }
+    }
+  }
 }
